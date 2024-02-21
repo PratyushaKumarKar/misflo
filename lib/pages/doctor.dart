8e6682a6c931c1bb9312418f1c5a31eca5b52d49
shriyash Jagtap <jagtapshriyash2@gmail.com>
2024-02-21 16:49:41 +0530

    updated doc

--- lib/pages/doctor.dart
+++ lib/pages/doctor.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:misflo/utils/screentools.dart';
 
@@ -71,70 +72,98 @@ class _DoctorsPageState extends State<DoctorsPage>
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          // Scrollable doctors section
-          Expanded(
-            flex:
-                1, // Adjust the flex factor to control the height of the doctor cards list relative to the tabs section
-            child: ListView.builder(
-              itemCount: doctors.length,
-              itemBuilder: (context, index) {
-                return DoctorCard(
-                  clinicName: doctors[index]['clinicName']!,
-                  doctorName: doctors[index]['doctorName']!,
-                  imageUrl: doctors[index]['imageUrl']!,
-                  onDelete: () => deleteDoctor(index),
-                );
-              },
-            ),
-          ),
-          // Fixed tabs section
-          Expanded(
-            flex:
-                1, // Adjust the flex factor to control the height of the tabs section relative to the doctor cards list
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  TabBar(
-                    controller: _tabController,
-                    labelColor: Colors.black,
-                    unselectedLabelColor: Colors.grey,
-                    indicatorColor: Colors.orange,
-                    tabs: [
-                      Tab(text: 'Appointments'),
-                      Tab(text: 'History'),
-                      Tab(text: 'Medication'),
-                    ],
-                  ),
-                  Expanded(
-                    child: TabBarView(
-                      controller: _tabController,
-                      children: [
-                        // Replace with actual content
-                        Center(child: Text('Appointments Content')),
-                        Center(child: Text('History Content')),
-                        Center(child: Text('Medication Content')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // Scrollable doctors section
+            Container(
+              height: height(context, 507),
+              child: Expanded(
+                flex:
+                    1, // Adjust the flex factor to control the height of the doctor cards list relative to the tabs section
+                child: Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.25),
+                          spreadRadius: -3,
+                          blurRadius: 10.3,
+                          offset:
+                              const Offset(1, -5), // changes position of shadow
+                        ),
                       ],
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(width(context, 35))),
+                      color: Colors.white),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemCount: doctors.length,
+                      itemBuilder: (context, index) {
+                        return DoctorCard(
+                          index: index,
+                          length: doctors.length,
+                          clinicName: doctors[index]['clinicName']!,
+                          doctorName: doctors[index]['doctorName']!,
+                          imageUrl: doctors[index]['imageUrl']!,
+                          onDelete: () => deleteDoctor(index),
+                        );
+                      },
                     ),
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-          //Old Float action button here
-        ],
+            // Fixed tabs section
+            Expanded(
+              flex:
+                  1, // Adjust the flex factor to control the height of the tabs section relative to the doctor cards list
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(width(context, 35))),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    TabBar(
+                      controller: _tabController,
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: Colors.orange,
+                      tabs: [
+                        Tab(text: 'Appointments'),
+                        Tab(text: 'History'),
+                        Tab(text: 'Medication'),
+                      ],
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        controller: _tabController,
+                        children: [
+                          // Replace with actual content
+                          Center(child: Text('Appointments Content')),
+                          Center(child: Text('History Content')),
+                          Center(child: Text('Medication Content')),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //Old Float action button here
+          ],
+        ),
       ),
     );
   }
@@ -145,6 +174,8 @@ class DoctorCard extends StatelessWidget {
   final String doctorName;
   final String imageUrl;
   final VoidCallback onDelete;
+  final int length;
+  final int index;
 
   const DoctorCard({
     Key? key,
@@ -152,45 +183,74 @@ class DoctorCard extends StatelessWidget {
     required this.doctorName,
     required this.imageUrl,
     required this.onDelete,
+    required this.length,
+    required this.index,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage(imageUrl),
-              radius: 30.0,
-            ),
-            SizedBox(height: 8.0),
-            Text(clinicName),
-            Text(doctorName, style: TextStyle(fontWeight: FontWeight.bold)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton.icon(
-                  icon: Icon(Icons.phone),
-                  label: Text('Call'),
-                  onPressed: () {
-                    // Implement call functionality
-                  },
-                  style: ElevatedButton.styleFrom(primary: Color(0xFFF4A223)),
+      elevation: 0,
+      child: Container(
+        height: height(context, 250),
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Stack(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(imageUrl),
+                radius: 52.0,
+              ),
+              Positioned(
+                  top: height(context, 26),
+                  left: width(context, 154),
+                  child: Text(clinicName)),
+              Positioned(
+                  left: width(context, 154),
+                  top: height(context, 56),
+                  child: Text(doctorName,
+                      style: TextStyle(fontWeight: FontWeight.bold))),
+              Positioned(
+                left: width(context, 10),
+                top: height(context, 133),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      child: Text('Call'),
+                      onPressed: () {
+                        // Implement call functionality
+                      },
+                      style:
+                          ElevatedButton.styleFrom(primary: Color(0xFFF4A223)),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    ElevatedButton(
+                      child: Text('Book an appointment'),
+                      onPressed: () {
+                        // Implement booking functionality
+                      },
+                      style:
+                          ElevatedButton.styleFrom(primary: Color(0xFFF4A223)),
+                    ),
+                  ],
                 ),
-                ElevatedButton.icon(
-                  icon: Icon(Icons.calendar_today),
-                  label: Text('Book an appointment'),
-                  onPressed: () {
-                    // Implement booking functionality
-                  },
-                  style: ElevatedButton.styleFrom(primary: Color(0xFFF4A223)),
-                ),
-              ],
-            ),
-          ],
+              ),
+              (index != (length - 1))
+                  ? Positioned(
+                      top: height(context, 210),
+                      child: Container(
+                        color: Colors.grey,
+                        width: width(context, 393),
+                        height: 1,
+                      ),
+                    )
+                  : Container()
+            ],
+          ),
         ),
       ),
     );
