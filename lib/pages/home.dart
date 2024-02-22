ba0802f78da19ad90d5b251d732484be529a311c
KaiShuro <20btcse86@suiit.ac.in>
2024-02-23 04:15:52 +0530

    log cramp button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -540,6 +540,39 @@ class _HomeState extends State<Home> {
                         height: height(context, 95),
                         width: width(context, 95),
                       ),
+                    )),
+                // ================================================= log cramps button
+                Positioned(
+                    left: width(context, 12),
+                    top: height(context, 700),
+                    child: InkWell(
+                      splashColor: Colors.transparent,
+                      onLongPress: () async {
+                        DocumentReference userDoc = FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(widget.user.uid);
+
+                        // Get the current timestamp
+                        Timestamp now = Timestamp.now();
+                        // Update the user's document by adding the new timestamp to the 'logCrampList' array
+                        // This uses the arrayUnion method to ensure the timestamp is added to the array without removing existing entries
+                        await userDoc.update({
+                          'logCrampList': FieldValue.arrayUnion([now]),
+                        }).then((_) {
+                          // If the update is successful, show a toast message
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Cramps Logged !')));
+                        }).catchError((error) {
+                          // If there is an error during the update, show a different toast message
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Cramps could not be logged')));
+                        });
+                      },
+                      child: Image.asset(
+                        "assets/logcramps.png",
+                        height: height(context, 95),
+                        width: width(context, 95),
+                      ),
                     ))
               ],
             ),
