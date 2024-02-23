4e5be685b739a71767c9d1b5900367b9f6b4476e
shriyash Jagtap <jagtapshriyash2@gmail.com>
2024-02-23 18:35:32 +0530

    extracted home widgets

--- lib/pages/daily_journal.dart
+++ lib/pages/daily_journal.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:misflo/utils/screentools.dart';
-import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import 'package:misflo/widgets/todolistview.dart';
 
 class DailyJournalPage extends StatefulWidget {
   const DailyJournalPage({super.key});
@@ -100,170 +100,13 @@ class _DailyJournalPageState extends State<DailyJournalPage>
                                     child: Stack(
                                       children: [
                                         Positioned(
-                                          top: height(context, 20),
-                                          left: 0,
-                                          right: 0,
-                                          child: Container(
-                                            width: MediaQuery.of(context)
-                                                .size
-                                                .width,
-                                            height: height(context, 130),
-                                            child: ListView.builder(
-                                              scrollDirection: Axis.horizontal,
-                                              itemCount:
-                                                  365, // Number of days to display
-                                              controller: _scrollController,
-                                              itemBuilder: (context, index) {
-                                                // Calculate date for current tile
-                                                var date = DateTime(
-                                                        DateTime.now().year,
-                                                        1,
-                                                        1)
-                                                    .add(Duration(days: index));
-                                                // Check if it's today's date
-                                                bool isToday = date
-                                                    .isAtSameMomentAs(DateTime(
-                                                        DateTime.now().year,
-                                                        DateTime.now().month,
-                                                        DateTime.now().day));
-                                                return Padding(
-                                                  padding: EdgeInsets.only(
-                                                      top: height(context, 3.0),
-                                                      bottom: height(
-                                                          context, 10.0)),
-                                                  child: GestureDetector(
-                                                    onTapDown: (details) {
-                                                      // Show a blur effect
-
-                                                      showMenu(
-                                                        context: context,
-                                                        position: RelativeRect
-                                                            .fromLTRB(
-                                                          details.globalPosition
-                                                              .dx,
-                                                          details.globalPosition
-                                                              .dy,
-                                                          details.globalPosition
-                                                              .dx,
-                                                          details.globalPosition
-                                                              .dy,
-                                                        ),
-                                                        items: [
-                                                          PopupMenuItem(
-                                                            child: Text(
-                                                                'Option 1'),
-                                                            onTap: () {
-                                                              // Handle option 1 tap
-                                                            },
-                                                          ),
-                                                          PopupMenuItem(
-                                                            child: Text(
-                                                                'Option 2'),
-                                                            onTap: () {
-                                                              // Handle option 2 tap
-                                                            },
-                                                          ),
-                                                        ],
-                                                      );
-                                                    },
-                                                    child: Container(
-                                                      width: width(context, 84),
-                                                      margin:
-                                                          EdgeInsets.symmetric(
-                                                              horizontal: width(
-                                                                  context, 4)),
-                                                      decoration: BoxDecoration(
-                                                        color: isToday
-                                                            ? Color(0xFFFFB459)
-                                                            : Colors.white,
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(width(
-                                                                    context,
-                                                                    14)),
-                                                        boxShadow: [
-                                                          BoxShadow(
-                                                            color: Colors.grey
-                                                                .withOpacity(
-                                                                    0.5),
-                                                            spreadRadius: 0,
-                                                            blurRadius: 4,
-                                                            offset: Offset(2,
-                                                                2), // changes position of shadow
-                                                          ),
-                                                        ],
-                                                      ),
-                                                      child: Stack(
-                                                        children: [
-                                                          Align(
-                                                            heightFactor:
-                                                                height(context,
-                                                                    20),
-                                                            alignment: Alignment
-                                                                .topCenter,
-                                                            child: Padding(
-                                                              padding: EdgeInsets
-                                                                  .only(
-                                                                      top: height(
-                                                                          context,
-                                                                          10)),
-                                                              child: Text(
-                                                                DateFormat(
-                                                                        'EEEE')
-                                                                    .format(
-                                                                        date)
-                                                                    .substring(
-                                                                        0, 3),
-                                                                style: GoogleFonts.quicksand(
-                                                                    fontSize: width(
-                                                                        context,
-                                                                        16),
-                                                                    fontWeight:
-                                                                        FontWeight
-                                                                            .bold),
-                                                              ),
-                                                            ),
-                                                          ),
-                                                          Center(
-                                                            child: Text(
-                                                              (DateFormat('dd').format(
-                                                                              date)[
-                                                                          0] ==
-                                                                      "0")
-                                                                  ? DateFormat(
-                                                                              'dd')
-                                                                          .format(
-                                                                              date)[
-                                                                      1]
-                                                                  : DateFormat(
-                                                                          'dd')
-                                                                      .format(
-                                                                          date), // Format date as 'MMM dd'
-                                                              style: GoogleFonts
-                                                                  .quicksand(
-                                                                fontSize: width(
-                                                                    context,
-                                                                    40),
-                                                                fontWeight:
-                                                                    FontWeight
-                                                                        .bold,
-                                                                color: isToday
-                                                                    ? Colors
-                                                                        .black
-                                                                    : Colors.grey[
-                                                                        800],
-                                                              ),
-                                                            ),
-                                                          ),
-                                                        ],
-                                                      ),
-                                                    ),
-                                                  ),
-                                                );
-                                              },
-                                            ),
-                                          ),
-                                        ),
+                                            top: height(context, 20),
+                                            left: 0,
+                                            right: 0,
+                                            child: TodoListview(
+                                                scrollController:
+                                                    _scrollController,
+                                                currentDate: DateTime.now())),
                                       ],
                                     ),
                                   ),
---
+++ lib/pages/diet2.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:misflo/pages/diet_recipies.dart';
+// Ensure you have 'screentools.dart' in your project for this to work.
+import 'package:misflo/utils/screentools.dart';
+
+class Diet2 extends StatefulWidget {
+  const Diet2({Key? key}) : super(key: key);
+
+  @override
+  State<Diet2> createState() => _Diet2State();
+}
+
+class _Diet2State extends State<Diet2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Positioned(
+            //   top: height(context, 2),
+            //   left: width(context, 5),
+            //   child: IconButton(
+            //     icon: const Icon(Icons.arrow_back_ios),
+            //     onPressed: () => Navigator.of(context).pop(),
+            //   ),
+            // ),
+
+            Positioned(
+              top: height(context, 184),
+              left: width(context, 5),
+              right: width(context, 5),
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(34),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 5,
+                            color: Colors.black.withOpacity(0.25),
+                            offset: Offset(1, -1))
+                      ]),
+                  height: height(context, 750),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: width(context, 20),
+                              top: height(context, 43),
+                              bottom: height(context, 31)),
+                          child: Text(
+                            'Search Results',
+                            style: GoogleFonts.poppins(
+                              fontSize: width(context, 25),
+                            ),
+                          ),
+                        ),
+                      ),
+                      _buildSearchResults(),
+                    ],
+                  )),
+            ),
+            Positioned(
+              top: height(context, 10),
+              left: width(context, 7),
+              right: width(context, 7),
+              child: Container(
+                  height: height(context, 189),
+                  width: width(context, 415),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(34),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 5,
+                            color: Colors.black.withOpacity(0.25),
+                            offset: Offset(1, -1))
+                      ]),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: height(context, 26),
+                            bottom: height(context, 0)),
+                        child: Row(children: [
+                          SizedBox(
+                            width: width(context, 22),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              print("back button press");
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 4,
+                                    color: Colors.black.withOpacity(0.25),
+                                    offset: Offset(-1, 1))
+                              ]),
+                              child: SvgPicture.asset(
+                                "assets/back_button_white.svg",
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: width(context, 19),
+                          ),
+                          _buildSearchBar()
+                        ]),
+                      ),
+                      SizedBox(
+                        height: height(context, 23),
+                      ),
+                      Container(
+                        height: height(context, 70),
+                        width: width(context, 386),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const DietRecipes()),
+                            );
+                          },
+                          child: Image.asset("assets/Explore_Banner.png",
+                              fit: BoxFit.fill),
+                        ),
+                      )
+                    ],
+                  )),
+            ),
+            Positioned(
+              left: 20,
+              right: 20,
+              bottom: 27,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle button tap for creating diet and workout plan
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                        bottomRight: Radius.circular(25)),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: height(context, 15)),
+                  child: Text(
+                    'Can' 't find your food? Make your own',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 244, 160, 35),
+                        fontSize: height(context, 20),
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Container(
+      height: height(context, 50),
+      width: width(context, 326),
+      padding: EdgeInsets.symmetric(
+          horizontal: width(context, 5), vertical: height(context, 8)),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(bottom: height(context, 12)),
+          hintText: 'Search',
+          hintStyle: GoogleFonts.lato(color: Colors.black),
+          prefixIcon: SvgPicture.asset("assets/Search.svg"),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchResults() {
+    return Expanded(
+      child: ListView(
+        children: [
+          _buildSearchResultItem('Egg'),
+          _buildSearchResultItem('Egg salad'),
+          _buildSearchResultItem('Egg Pav'),
+          _buildSearchResultItem('Egg Pie'),
+          // Add more items as required
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchResultItem(String title) {
+    return ListTile(
+      title: Text(title),
+      trailing: IconButton(
+        icon: Icon(Icons.add, color: Colors.yellow),
+        onPressed: () {
+          // Handle the action when the plus icon is tapped
+        },
+      ),
+    );
+  }
+}
